557f1b6d0ba0b716036d6ff0ebcd7b6f4a6e4b6f
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-20 09:43:23 +0300

    update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,13 +5,6 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
-#set env vars
-set -o allexport; source .env; set +o allexport;
-
-#wait until the server is ready
-echo "Waiting for software to be ready ..."
-# sleep 30s;
-
 curl https://${BASE_URL}/i/index.php?step=0 \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
@@ -21,7 +14,7 @@ curl https://${BASE_URL}/i/index.php?step=0 \
   -H 'priority: u=0, i' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
-  --data-raw 'language=fr'
+  --data-raw 'language=en'
 
 
 curl https://${BASE_URL}/i/index.php?step=1 \
@@ -65,7 +58,7 @@ curl https://${BASE_URL}/i/index.php?step=4 \
   -H 'priority: u=0, i' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
-  --data-raw 'language=fr'
+  --data-raw 'language=en'
 
 curl https://${BASE_URL}/i/index.php?step=5 \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
@@ -76,4 +69,4 @@ curl https://${BASE_URL}/i/index.php?step=5 \
   -H 'priority: u=0, i' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
-  --data-raw 'language=fr'
+  --data-raw 'language=en'
